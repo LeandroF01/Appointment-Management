6491d06cd89074091ce86f2b830408aa29a4a750
LeandroF01 <leaylio3327@gmail.com>
2026-02-05 10:43:05 -0300

    Add: module and services prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { EventModule } from './modules/event/event.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
